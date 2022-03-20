0fd50c68d5345dbf80b34cdf36d6f2d78ea5c64c
systeno <51444175+systeno@users.noreply.github.com>
2022-03-20 15:37:31 +0000

    GitHub Pages Deploy

---
+++ assets/AboutView.7478c570.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a6cb4d86.js";import{o as t,c as o,a as _}from"./vendor.00c8e97e.js";const s={},a={class:"about"},c=_("h1",null,"This is the new about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
