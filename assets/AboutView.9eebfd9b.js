ebe802c4e98473cfc40812de067fa20c9cea806b
systeno <51444175+systeno@users.noreply.github.com>
2022-03-20 15:31:51 +0000

    Deploying to gh-pages from @ systeno/portfolio@084fe4f8cbde1f8372a6cdb7dd029246a540c075 🚀

--- assets/AboutView.9eebfd9b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.e7456b54.js";import{o as t,c as o,a as _}from"./vendor.00c8e97e.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
